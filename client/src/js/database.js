d1c282064dce5850c4e2ff9e24a6b7794e84009a
Ryan McIntyre <rmcintyre598@gmail.com>
2024-03-02 23:15:57 -0500

    fixed some files

---
+++ client/src/js/database.js
@@ -0,0 +1,65 @@
+import { openDB } from "idb";
+
+const initdb = async () =>
+  // Creates a new database named 'kitties' which will be using version 1 of the database.
+  openDB("jate", 1, {
+    // Adds database schema if it has not already been initialized.
+    upgrade(db) {
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("Database for jate already exists");
+        return;
+      }
+      // Creates a new object store for the data and gives it a key name of 'id' which increments automatically.
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("Database for jate has been created");
+    },
+  });
+
+// Exports a function to POST to the database.
+export const putDb = async (content) => {
+  console.log("Post to the database");
+
+  // Creates a connection to the kitties database and version.
+  const kittiesDb = await openDB("jate", 1);
+
+  // Creates a new transaction and specifies the database and data privileges.
+  const tx = kittiesDb.transaction("jate", "readwrite");
+
+  // Opens up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Uses the .put() method on the store and passes in the content.
+  const request = store.put({ id: 1, jate: content });
+
+  // Gets confirmation of the request.
+  const result = await request;
+  console.log("Data saved to the database:", result.values);
+};
+
+// Exports a function to get the database.
+export const getDb = async () => {
+  console.log("Get all notes from the database");
+
+  // Creates a connection to the kitties database and version.
+  const kittiesDb = await openDB("jate", 1);
+
+  // Creates a new transaction and specifies the database and data privileges.
+  const tx = kittiesDb.transaction("jate", "readonly");
+
+  // Opens up the desired object store.
+  const store = tx.objectStore("jate");
+
+  // Uses the .get(1) method to retrieve the value of the first record matching the query.
+
+  const request = store.get(1);
+
+  // Gets confirmation of the request.
+  const result = await request;
+  result
+    ? console.log("Notes retrieved from database:", result.jate)
+    : console.log("No notes found in database!");
+  return result?.jate;
+};
+
+// Starts database
+initdb();
---
+++ client/webpack.config.js
@@ -0,0 +1,80 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+// Require the InjectManifest class of the WorkBoxPlugin
+const { InjectManifest } = require("workbox-webpack-plugin");
+
+module.exports = () => {
+  return {
+    mode: "development",
+    entry: {
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+    },
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      // Webpack plugin that generates the html file and injects the bundles.
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Jate",
+        favicon: "./favicon.ico",
+      }),
+      // Injects the custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just another text editor",
+        short_name: "J.A.T.E.",
+        description: "A secure place to keep thoughts!",
+        favicon: "./favicon.ico",
+        // Theme and background color pulled from duotone-light.min.css
+        background_color: "#faf8f5",
+        theme_color: "#b29762",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logoNew.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
+
+    module: {
+      rules: [
+        // CSS loader
+        // Reminder- An 'i' after a regular expression specifies
+        // that the test requires a case-insensitive match
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // Babel-loader in order to use ES6.
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
